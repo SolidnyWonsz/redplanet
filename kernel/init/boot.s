77a84152ad862448778e0c643ae4878719b11f57
Solidny Wonsz <mazurekdominik06@gmail.com>
2024-06-11 17:49:41 +0200

    New commit

---
+++ kernel/init/boot.s
@@ -0,0 +1,36 @@
+.set ALIGN,		1<<0
+.set MEMINFO,	1<<1
+.set FLAGS,		ALIGN | MEMINFO
+.set MAGIC,		0x1BADB002
+.set CHECKSUM,	-(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384
+stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov $stack_top, %esp
+
+	/*
+	Since Red Planet has moved to C++, paging, GDT, global constructors,
+	floating point instructions and exceptions must be initialized here.
+	*/
+
+	call kernel_init
+
+	cli
+1:	hlt
+	jmp 1b
+
+.size _start, . - _start
